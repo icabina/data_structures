1979ab56f1ba2cd29fc7b7bbe8e16bc7e3117560
Jorge Roldán <icabina@live.com>
2022-03-15 12:10:02 -0500

    2022

--- js/binary_tree_search.js
+++ js/binary_tree_search.js
@@ -129,6 +129,97 @@ class BTS {
     };
     this.root = removeNode(this.root, data);
   }
+
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+  findMinHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  findMaxHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  inOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traverseInOrder(node) {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+  preOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePreOrder(node) {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+  postOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
+
+  levelOrder() {
+    let result = [];
+    let Q = [];
+    if (this.root != null) {
+      Q.push(this.root);
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+        if (node.left != null) {
+          Q.push(node.left);
+        }
+        if (node.right != null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
 }
 console.log("=========================================");
 console.log("new binary search tree");
@@ -147,3 +238,28 @@ console.log(bts.findMin());
 console.log(bts.findMax());
 bts.remove(7);
 console.log(bts.isPresent(4));
+
+bts.add(9);
+bts.add(4);
+bts.add(17);
+bts.add(3);
+bts.add(6);
+bts.add(22);
+bts.add(5);
+bts.add(7);
+bts.add(20);
+
+console.log(bts.findMinHeight());
+console.log(bts.findMaxHeight());
+console.log(bts.isBalanced());
+bts.add(10);
+console.log(bts.findMinHeight());
+console.log(bts.findMaxHeight());
+console.log(bts.isBalanced());
+
+//traversal
+console.log("inOrder: " + bts.inOrder());
+console.log("preOrder: " + bts.preOrder());
+console.log("postOrder: " + bts.postOrder());
+
+console.log("levelOrder: " + bts.levelOrder());
